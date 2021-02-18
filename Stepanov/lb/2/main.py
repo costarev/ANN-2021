4d4c86086821cadfd198311c62fadf563e5de467
Vlad <vlad@MacBook-Pro-Vlad.local>
2021-02-18 20:29:40 +0300

    lb2

---
+++ Stepanov/lb/2/main.py
@@ -0,0 +1,50 @@
+import pandas
+from tensorflow.keras.layers import Dense
+from tensorflow.keras.models import Sequential
+from tensorflow.keras.utils import to_categorical
+from sklearn.preprocessing import LabelEncoder
+
+import matplotlib.pyplot as plt
+
+dataframe = pandas.read_csv("sonar.csv", header=None)
+dataset = dataframe.values
+X = dataset[:,0:30].astype(float)
+Y = dataset[:,60]
+
+# transform data from "R" and "M" to 0 and 1
+encoder = LabelEncoder()
+encoder.fit(Y)
+encoded_Y = encoder.transform(Y)
+
+model = Sequential()
+model.add(Dense(60, input_dim=30, activation='relu'))
+model.add(Dense(30, activation='relu'))
+model.add(Dense(1, activation='sigmoid'))
+
+model.compile(optimizer='adam', loss='binary_crossentropy', metrics=['accuracy'])
+
+H = model.fit(X, encoded_Y, epochs=100, batch_size=10, validation_split=0.1)
+
+#Получение ошибки и точности в процессе обучения
+loss = H.history['loss']
+val_loss = H.history['val_loss']
+acc = H.history['accuracy']
+val_acc = H.history['val_accuracy']
+epochs = range(1, len(loss) + 1)
+#Построение графика ошибки
+plt.plot(epochs, loss, 'bo', label='Training loss')
+plt.plot(epochs, val_loss, 'b', label='Validation loss')
+plt.title('Training and validation loss')
+plt.xlabel('Epochs')
+plt.ylabel('Loss')
+plt.legend()
+plt.show()
+#Построение графика точности
+plt.clf()
+plt.plot(epochs, acc, 'bo', label='Training acc')
+plt.plot(epochs, val_acc, 'b', label='Validation acc')
+plt.title('Training and validation accuracy')
+plt.xlabel('Epochs')
+plt.ylabel('Accuracy')
+plt.legend()
+plt.show()
