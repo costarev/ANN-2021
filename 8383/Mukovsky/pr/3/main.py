2247e67976987e945042f72494eb44fb98b13d35
garet2gis <br-avm32@yandex.ru>
2021-03-11 12:36:13 +0300

    Fixed indexes in a loop

--- 8383/Mukovsky/pr/3/main.py
+++ 8383/Mukovsky/pr/3/main.py
@@ -7,7 +7,7 @@ def get_tensor(radius, color):
     size = radius * 2
     center = (size - 1) / 2
     tensor = np.zeros((size, size, 3), dtype='int')
-    for ind in np.ndindex(size, size, 3):
+    for ind in np.ndindex(size, size):
         if math.sqrt((center - ind[1]) ** 2 + (center - ind[0]) ** 2) <= radius:
             tensor[ind[0]][ind[1]] = color
     return tensor
